3c7851576dcb7e8f14d82b57bbc70ddaf1c9a589
yadav <yadavharshit311@gmail.com>
2023-06-23 14:40:16 +0530

    added

---
+++ 3.py
@@ -0,0 +1,16 @@
+""" Given the participants score-sheet for your University Sports Day, you are required to
+ find the runner-up score. You are given  scores. Store them in a list and find the score of
+ the runner-up.
+Input Format
+The first line contains . The second line contains an array   of  integers each separated by a space.
+Print the runner-up score."""
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    li = []
+    for i in arr:
+        li.append(i)
+    print(li)
+    res=[*set(li)]
+    res.sort()
+    print(res[-2])
